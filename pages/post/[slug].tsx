b6c4deef418216f2db332622211d57d4a32cf708
mcenny01 <dev.mcenny@gmail.com>
2022-04-09 02:13:13 +0100

    I added the comment section to the post

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,42 +1,215 @@
-// import Header from '../../components/Header'
-// import { sanityClient } from '../../sanity'
-// import { Post } from '../../typings'
-// import { GetStaticProps } from 'next'
+import Header from '../../components/Header'
+import { sanityClient, urlFor } from '../../sanity'
+import { Post } from '../../typings'
+import { GetStaticProps } from 'next'
+import PortableText from 'react-portable-text'
+import { useForm, submitHandler } from 'react-hook-form'
 
-// interface Props {
-//   post: Post
-// }
+interface IFormInput {
+  _id: string
+  name: string
+  email: string
+  comment: string
+}
 
-// export default function Post({ post }: Props) {
-//   return (
-//     <main>
-//       <Header />
-//     </main>
-//   )
-// }
+interface Props {
+  post: Post
+}
 
-// export const getStaticPaths = async () => {
-//   const query = `*[_type == "post"]{
-//         _id,
-//         slug{
-//             current
-//         }
-//     }`
-//   const posts = await sanityClient.fetch(query)
+function Post({ post }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>()
 
-//   const paths = posts.map((post: Post) => ({
-//     params: {
-//       slug: post.slug.current,
-//     },
-//   }))
+  const onSubmit: submitHandler<IFormInput> = async (data) => {
+    await fetch('/api/createComment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
-//   return {
-//     paths,
-//     fallback: 'blocking',
-//   }
-// }
+  return (
+    <main>
+      <Header />
+      <img
+        src={urlFor(post.mainImage).url()!}
+        className="h-40 w-full object-cover"
+        alt=""
+      />
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+        <div className="mt-5 flex items-center gap-2">
+          <img
+            src={urlFor(post.author.image).url()!}
+            className="h-10 w-10 rounded-full"
+            alt=""
+          />
+          <p className="text-sm font-extralight">
+            Blog post by{' '}
+            <span className="font-light text-green-600">
+              {post.author.name}
+            </span>{' '}
+            - Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+        <div className="mt-10">
+          <PortableText
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="my-5 text-2xl font-bold" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="my-5 text-xl font-bold" {...props} />
+              ),
+              p: (props: any) => <p className="text-sm " {...props} />,
+              li: ({ children }: any) => (
+                <li className="ml-10 mt-3 mb-3 list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
 
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
+      <hr className="my-5 mx-auto max-w-3xl border border-yellow-500" />
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mx-auto mb-10 flex max-w-2xl flex-col p-5"
+      >
+        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+        <hr className="mt-2 py-3" />
+
+        <input {...register('_id')} type="hidden" name="_id" value={post._id} />
+
+        <label className="mb-5 block">
+          <span className="text-gray-700">Name</span>
+          <input
+            {...register('name', { required: true })}
+            placeholder="John Doe"
+            className="w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring-1"
+            type="text"
+          />
+        </label>
+        <label className="mb-5 block">
+          <span className="text-gray-700">Email</span>
+          <input
+            {...register('email', { required: true })}
+            placeholder="name@email.com"
+            className="w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring-1"
+            type="email"
+          />
+        </label>
+        <label className="mb-5 block">
+          <span className="text-gray-700">Comment</span>
+          <textarea
+            {...register('comment', { required: true })}
+            placeholder="Comment"
+            className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring-1"
+            rows={8}
+          />
+        </label>
+
+        <div className="flex flex-col p-5">
+          {errors.name && (
+            <span className="text-red-500">- The name field is required</span>
+          )}
+          {errors.name && (
+            <span className="text-red-500">- The email field is required</span>
+          )}
+          {errors.name && (
+            <span className="text-red-500">
+              - The comment field is required
+            </span>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          className="focus:shadow-outline cursor-pointer rounded bg-yellow-500 py-2 px-4 font-bold text-white shadow hover:bg-yellow-400 focus:outline-none"
+        />
+      </form>
+    </main>
+  )
+}
+
+export default Post
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
+        _id,
+        slug{
+            current
+        }
+    }`
+  const posts = await sanityClient.fetch(query)
+
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }))
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        title,
+        author -> {
+            name,
+            image
+        },
+        'comments': *[_type == "comment" && post._ref == ^._id && approved == true],
+        description,
+        mainImage,
+        slug,
+        body
+    }`
+
+  const post = await sanityClient.fetch(query, { slug: params?.slug })
+
+  if (!post) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60,
+  }
+}
+
+// export async  function getStaticProps({slug}:Post) {
+
 //   const query = `*[_type == "post" && slug.current == $slug][0]{
 //         _id,
 //         _createdAt,
@@ -51,7 +224,9 @@
 //         slug,
 //         body
 //     }`
-//   const post = await sanityClient.fetch(query, { slug: params?.slug })
+
+//   const post = await sanityClient.fetch(query, { slug: slug })
+
 //   if (!post) {
 //     return {
 //       notFound: true,
