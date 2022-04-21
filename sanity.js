63793410f68d79e77595834810038d3d7cbaeca6
mcenny01 <dev.mcenny@gmail.com>
2022-04-21 13:51:32 +0100

    fix

--- sanity.js
+++ sanity.js
@@ -3,7 +3,7 @@ import createClient from '@sanity/client'
 import { createCurrentUserHook } from 'next-sanity'
 
 export const config = {
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: 'vt85il9r',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2021-03-25',
   useCdn: process.env.NODE_ENV === 'production',
